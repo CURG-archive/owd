baf32676e596e52ba15ce8583b16e96f4ede5b6a
jvarley <armuser@quemoy.(none)>
2014-02-05 15:23:40 -0500

    Catkinized package Paramatrized build so that not all targets are built each time. Currently only barrett hand driver is compiled Got rid of a whole lot of compiled files  -- Deleted build dir  -- Deleted devel dir  -- Deleted binaries in source dir

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/armuser/ros/rosbuild_src/owd
-# Build directory: /home/armuser/ros/rosbuild_src/owd/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(openwam)
-SUBDIRS(openmath)
--- build/openmath/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/armuser/ros/rosbuild_src/owd/openmath
-# Build directory: /home/armuser/ros/rosbuild_src/owd/build/openmath
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
